d2c8685bcd7760284d30ca627a5a18d0316bc3d3
uminor <minor248@sj9.so-net.ne.jp>
2017-12-10 20:41:13 +0900

    first

---
+++ complex3.py
@@ -0,0 +1,143 @@
+﻿#
+# "xor.py"
+#
+#  an example of keras (with tensorflow by Google)
+#   by U.minor
+#    free to use with no warranty
+#
+# usage:
+# python radioactive_area.py 5000
+#
+# last number (5000) means learning epochs, default=1000 if omitted
+
+import tensorflow as tf
+import keras
+from keras.optimizers import SGD
+import numpy as np
+from numpy.random import *
+import matplotlib.pyplot as plt
+import sys
+import time
+
+argvs = sys.argv
+
+i_train, o_train = [], []
+
+sample = 9 # 100
+
+# generate sample data
+#for i in range(sample):
+#	x = randint(0, 2)
+#	y = randint(0, 2)
+#	c = x ^ y
+
+#	i_train.append([x * 1.0, y * 1.0])
+#	o_train.append(c * 1.0)
+
+for i in range(3):
+	for j in range(3):
+		i_train.append([i*1.0, j*1.0])
+		o_train.append((i%2)^(j%2))
+
+print(i_train)
+print(o_train)
+
+#sys.exit()
+
+xps, yps = [], []
+for i in range(sample):
+	if o_train[i] < 0.5:
+		xps.append(i_train[i][0])
+		yps.append(i_train[i][1])
+
+xns, yns = [], []
+for i in range(sample):
+	if o_train[i] >= 0.5:
+		xns.append(i_train[i][0])
+		yns.append(i_train[i][1])
+
+## if plot input image
+#plt.scatter(xps, yps, c="blue", marker="*", s=100)
+#plt.scatter(xns, yns, c="red", marker="*", s=200)
+#plt.show()
+
+#sys.exit()
+
+
+from keras.layers import Dense, Activation
+model = keras.models.Sequential()
+
+# neural network model parameters
+hidden_units = 3
+layer_depth = 2
+act =  'sigmoid' # 'relu' #
+bias = True
+
+# first hidden layer
+model.add(Dense(units = hidden_units, input_dim = 2, use_bias = bias))
+model.add(Activation(act))
+
+# additional hidden layers (if necessary)
+for i in range(layer_depth - 1):
+	model.add(Dense(units = hidden_units, input_dim = hidden_units, use_bias = bias))
+	model.add(Activation(act))
+
+# output layer
+model.add(Dense(units = 2, use_bias = bias))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+# training
+if len(argvs) > 1 and argvs[1] != '':
+	ep = int(argvs[1]) # from command line
+else:
+	ep = 1000
+
+start_fit = time.time()
+model.fit(i_train, o_train, epochs = ep, verbose = 1)
+elapsed = time.time() - start_fit
+print("elapsed = {:.1f} sec".format(elapsed))
+
+#score = model.evaluate(x_test, y_test, batch_size = 1)
+#print("accuracy=", score[1])
+
+# predict
+ticks = 40
+a = []
+for ix in range(ticks):
+	for iy in range(ticks):
+		a.append([ix * 4.0 / ticks - 0.5, iy * 4.0 / ticks - 0.5])
+
+p = np.array(a)
+
+r = model.predict(p)
+print(r)
+
+thresh = 0.5
+
+# safe area (blue points)
+xp, yp = [], []
+for i in range(ticks ** 2):
+	if r[i][1] < thresh:
+		xp.append(p[i][0])
+		yp.append(p[i][1])
+
+plt.scatter(xp, yp, c="cyan", marker=".")
+
+# dangerous area (red points)
+xn, yn = [], []
+for i in range(ticks ** 2):
+	if r[i][1] >= thresh:
+		xn.append(p[i][0])
+		yn.append(p[i][1])
+
+plt.scatter(xn, yn, c="magenta", marker=".")
+
+plt.scatter(xps, yps, c="blue", marker="*", s=100)
+plt.scatter(xns, yns, c="red", marker="*", s=200)
+
+plt.show()
+
+
---
+++ complex4.py
@@ -0,0 +1,140 @@
+﻿#
+# "xor.py"
+#
+#  an example of keras (with tensorflow by Google)
+#   by U.minor
+#    free to use with no warranty
+#
+# usage:
+# python radioactive_area.py 5000
+#
+# last number (5000) means learning epochs, default=1000 if omitted
+
+import tensorflow as tf
+import keras
+from keras.optimizers import SGD
+import numpy as np
+from numpy.random import *
+import matplotlib.pyplot as plt
+import sys
+import time
+
+argvs = sys.argv
+
+i_train, o_train = [], []
+
+sample = 16+1
+
+for i in range(4):
+	for j in range(4):
+		i_train.append([i*1.0, j*1.0])
+		if i==2 and j==1 or i==1 and j==2:
+			o_train.append(0.0)
+		else:
+			o_train.append((i%2)^(j%2))
+
+i_train.append([1.5, 1.5])
+o_train.append(1.0)
+
+print(i_train)
+print(o_train)
+
+#sys.exit()
+
+xps, yps = [], []
+for i in range(sample):
+	if o_train[i] < 0.5:
+		xps.append(i_train[i][0])
+		yps.append(i_train[i][1])
+
+xns, yns = [], []
+for i in range(sample):
+	if o_train[i] >= 0.5:
+		xns.append(i_train[i][0])
+		yns.append(i_train[i][1])
+
+## if plot input image
+#plt.scatter(xps, yps, c="blue", marker="*", s=100)
+#plt.scatter(xns, yns, c="red", marker="*", s=200)
+#plt.show()
+
+#sys.exit()
+
+
+from keras.layers import Dense, Activation
+model = keras.models.Sequential()
+
+# neural network model parameters
+hidden_units = 6
+layer_depth = 2
+act =  'sigmoid' # 'relu' #
+bias = True
+
+# first hidden layer
+model.add(Dense(units = hidden_units, input_dim = 2, use_bias = bias))
+model.add(Activation(act))
+
+# additional hidden layers (if necessary)
+for i in range(layer_depth - 1):
+	model.add(Dense(units = hidden_units, input_dim = hidden_units, use_bias = bias))
+	model.add(Activation(act))
+
+# output layer
+model.add(Dense(units = 2, use_bias = bias))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+# training
+if len(argvs) > 1 and argvs[1] != '':
+	ep = int(argvs[1]) # from command line
+else:
+	ep = 1000
+
+start_fit = time.time()
+model.fit(i_train, o_train, epochs = ep, verbose = 1)
+elapsed = time.time() - start_fit
+print("elapsed = {:.1f} sec".format(elapsed))
+
+#score = model.evaluate(x_test, y_test, batch_size = 1)
+#print("accuracy=", score[1])
+
+# predict
+ticks = 40
+a = []
+for ix in range(ticks):
+	for iy in range(ticks):
+		a.append([ix * 6.0 / ticks - 1.0, iy * 6.0 / ticks - 1.0])
+
+p = np.array(a)
+
+r = model.predict(p)
+print(r)
+
+thresh = 0.5
+
+# safe area (blue points)
+xp, yp = [], []
+for i in range(ticks ** 2):
+	if r[i][1] < thresh:
+		xp.append(p[i][0])
+		yp.append(p[i][1])
+
+plt.scatter(xp, yp, c="cyan", marker=".")
+
+# dangerous area (red points)
+xn, yn = [], []
+for i in range(ticks ** 2):
+	if r[i][1] >= thresh:
+		xn.append(p[i][0])
+		yn.append(p[i][1])
+
+plt.scatter(xn, yn, c="magenta", marker=".")
+
+plt.scatter(xps, yps, c="blue", marker="*", s=100)
+plt.scatter(xns, yns, c="red", marker="*", s=200)
+
+plt.show()
+
+
